4c046d2fc08da12a017c71523c925d82421b72a6
Dmitriy Katin <dmitriykatinn@gmail.com>
2024-03-04 15:48:23 +0300

    Реализовал валидацию

--- src/main/java/ru/jsms/backend/security/domain/JwtRequest.java
+++ src/main/java/ru/jsms/backend/security/domain/JwtRequest.java
@@ -3,11 +3,17 @@ package ru.jsms.backend.security.domain;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
 @Setter
 @Getter
 public class JwtRequest {
-
+    @NotNull
+    @Email
     private String email;
+    @NotEmpty
     private String password;
 
 }
--- src/main/java/ru/jsms/backend/security/domain/RegisterRequest.java
+++ src/main/java/ru/jsms/backend/security/domain/RegisterRequest.java
@@ -4,12 +4,21 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class RegisterRequest {
+    @NotNull
+    @Email
     private String email;
+    @NotEmpty
     private String password;
+    @NotEmpty
     private String firstName;
+    @NotEmpty
     private String secondName;
 }
